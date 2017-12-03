4c368463b857501243dc02ac5a92673fcaff3832
Dallas williams <dwilliamsl80@yahoo.com>
2017-12-03 02:05:40 -0600

    Added Order history page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,5 +2,6 @@ class CartsController < ApplicationController
 	before_action :authenticate_user!
   def show
     @order_items = current_order.order_items
+    @invoices = current_user.invoices
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :books
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
+  resources :invoices
+  resources :orders
   mount RailsAdmin::Engine => '/admin', as: :rails_admin
 
   root 'home#index'
-  get 'pages/about'
 end
