a0171d1e40363f02733a23e0e674a956dbb13243
Dallas williams <dwilliamsl80@yahoo.com>
2017-12-03 23:16:19 -0600

    Updated CSS

--- db/migrate/20171013014215_import_data.rb
+++
@@ -1,5 +0,0 @@
-class ImportData < ActiveRecord::Migration[5.0]
-  def up
-  Rake::Task['db:data:load'].invoke
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202073550) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "authors", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "first_name", limit: 200
